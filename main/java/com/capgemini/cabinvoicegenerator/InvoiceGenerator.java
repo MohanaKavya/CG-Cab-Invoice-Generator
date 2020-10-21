fc4d66ce8bc6071c029ec93796021c2ac819fa50
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-10-21 11:44:47 +0530

    Added : Welcome Message is Added

---
+++ main/java/com/capgemini/cabinvoicegenerator/InvoiceGenerator.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.capgemini.cabinvoicegenerator;
+
+import java.util.logging.Logger;
+
+/**
+ * @author Mohana Kavya
+ *
+ */
+public class InvoiceGenerator {
+	public static void main(String[] args) {
+		Logger log = Logger.getLogger(InvoiceGenerator.class.getName());
+		log.info("Welcome to the Cab Invoice Generator System.");
+	}
+}
